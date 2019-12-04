4ab6ed2286582c8da357b78b7bab46da98af25b8
Hamza Astid <hastid@e2r10p11.1337.ma>
2019-12-04 05:10:19 +0100

    advance *

--- src/sh_etoil.c
+++ src/sh_etoil.c
@@ -6,7 +6,7 @@
 /*   By: hastid <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 00:43:40 by hastid            #+#    #+#             */
-/*   Updated: 2019/12/04 03:44:25 by hastid           ###   ########.fr       */
+/*   Updated: 2019/12/04 05:05:32 by hastid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,46 +25,73 @@ static int	match(char *s1, char *s2)
 	return (0);
 }
 
-static int	save_etoil(t_tok **toks, char *str, int id)
+static char	*search_path(char *str)
+{
+	int		i;
+	int		end;
+	char	*path;
+
+	i = 0;
+	end = 0;
+	while (str[i])
+	{
+		if (str[i] == '/')
+			end = i;
+		i++;
+	}
+	path = end ? ft_strsub(str, 0, end + 1) : ft_strdup(".");
+	return (path);
+}
+
+static int	add_token(char *str, t_tok **toks, char *path)
 {
 	DIR				*dirp;
+	char			*tmp;
 	struct dirent	*dp;
 	int				check;
 
 	check = 0;
-	dirp = opendir(".");
+	dirp = opendir(path);
 	if (!dirp)
-		return (1);
+		return (-1);
 	while ((dp = readdir(dirp)))
-		if (dp->d_name[0] != '.' && match(dp->d_name, str))
+	{
+		tmp = ft_strcmp(".", path) ? ft_strjoin(path, dp->d_name)
+			: ft_strdup(dp->d_name);
+		if (dp->d_name[0] != '.' && match(tmp, str))
 		{
 			check = 1;
-			if (save_tokens(toks, dp->d_name, id))
-				return (1);
+			if (save_tokens(toks, tmp, 0))
+				return (-1);
 		}
-	if (!check)
-		if (save_tokens(toks, str, id))
-			return (1);
+		ft_memdel((void **)&tmp);
+	}
 	closedir(dirp);
-	return (0);
+	return (check);
 }
 
-static int	check_etoil(char *str)
+static int	save_etoil(t_tok **toks, char *str)
 {
-	int	i;
+	int		ret;
+	char	*path;
 
-	i = 0;
-	while (str[i])
-		if (str[i++] == '*')
+	if (!(path = search_path(str)))
+		return (1);
+	ret = add_token(str, toks, path);
+	if (ret == -1)
+		return (1);
+	if (!ret)
+		if (save_tokens(toks, str, 0))
 			return (1);
+	ft_memdel((void **)&path);
 	return (0);
 }
 
 int			edit_etoile(t_tok **toks, char *str, int id)
 {
-	if (check_etoil(str))
+	if (ft_strchr(str, '*'))
 	{
-		if (save_etoil(toks, str, 0))
+		if (save_etoil(toks, str))
 			return (1);
 	}
 	else if (save_tokens(toks, str, id))
