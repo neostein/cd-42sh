a33c6123a093b8be3225273ee34656f3c2b0a759
Hamza Astid <hastid@e2r10p11.1337.ma>
2019-12-06 13:08:38 +0100

    setenv err

--- src/sh_environ.c
+++ src/sh_environ.c
@@ -6,7 +6,7 @@
 /*   By: hastid <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 03:39:43 by hastid            #+#    #+#             */
-/*   Updated: 2019/12/02 02:52:05 by hastid           ###   ########.fr       */
+/*   Updated: 2019/12/06 13:07:28 by hastid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,20 @@ int		ft_unsetenv(t_env **env, char **args)
 
 int		ft_setenv(t_env **env, char **args)
 {
+	int	i;
+
 	if (args[1])
 	{
+		if (!ft_isalpha(args[1][0]) && args[1][0] != '_')
+			return (ft_perror(0, "Variable name must begin with a letter", 1));
+		i = 0;
+		while (args[1][++i])
+			if (!ft_isalnum(args[1][i]) && args[1][i] != '_')
+			{
+				return (ft_perror(0,
+						"Variable name must contain alphanumeric characters.",
+						1));
+			}
 		if (add_elem(env, args[1], args[2]))
 			return (1);
 	}
